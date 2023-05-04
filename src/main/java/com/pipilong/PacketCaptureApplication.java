5bb6ad181400e20d6947ca27b4f6492c1ff0fd98
ZSL <2534158602@qq.com>
2023-05-04 09:37:48 +0800

    first

---
+++ src/main/java/com/pipilong/PacketCaptureApplication.java
@@ -0,0 +1,13 @@
+package com.pipilong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketCaptureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacketCaptureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pipilong/PacketCaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipilong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketCaptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
